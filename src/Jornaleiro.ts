70c8187b31d57f13d6b21da0548e4cecdc64139c
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2022-02-26 21:02:49 -0300

    Implementa uso de Carteira para Jornaleiro

--- src/Jornaleiro.ts
+++ src/Jornaleiro.ts
@@ -1,12 +1,16 @@
 import Revista from "./Revista"
+import Carteira from "./Carteira"
+import MeioDePagamento from "./MeioDePagamento"
 
 export default class Jornaleiro {
   private nome: string
   private revistas: Revista[]
+  private carteira: Carteira
 
-  constructor(nome: string) {
+  constructor(nome: string, carteira: Carteira) {
     this.nome = nome;
     this.revistas = []
+    this.carteira = carteira
   }
 
   public addRevista(revista: Revista): void {
@@ -15,5 +19,17 @@ export default class Jornaleiro {
 
   public getRevistas(): Revista[] {
     return this.revistas
+  }
+
+  public getSaldo(): number {
+    return this.carteira.getTotal()
+  }
+
+  public getNome(): string {
+    return this.nome
+  }
+
+  public getMeiosDePagamento(): MeioDePagamento[] {
+    return this.carteira.getMeiosDePagamento()
   }
 }
--- src/tests/jornaleiro.spec.ts
+++ src/tests/jornaleiro.spec.ts
@@ -1,16 +1,48 @@
 import Jornaleiro from '../Jornaleiro';
 import Revista from '../Revista';
+import Carteira from '../Carteira';
+import Pix from '../Pix';
 
 describe('Jornaleiro', () => {
   it('Deve criar um Jornaleiro', () => {
-    const jornaleiro = new Jornaleiro('João')
+    const carteira = new Carteira()
+    const pix = new Pix(100)
+    carteira.addMeioDePagamento(pix)
+    const jornaleiro = new Jornaleiro('João', carteira)
     expect(jornaleiro).toBeDefined()
   })
 
+  it('Deve mostrar o nome do Jornaleiro', () => {
+    const carteira = new Carteira()
+    const pix = new Pix(100)
+    carteira.addMeioDePagamento(pix)
+    const jornaleiro = new Jornaleiro('João', carteira)
+    expect(jornaleiro.getNome()).toBe('João')
+  })
+
   it('Deve adicionar uma revista', () => {
-    const jornaleiro = new Jornaleiro('João')
+    const carteira = new Carteira()
+    const pix = new Pix(100)
+    carteira.addMeioDePagamento(pix)
+    const jornaleiro = new Jornaleiro('João', carteira)
     const revista = new Revista('Receitas', 10)
     jornaleiro.addRevista(revista)
     expect(jornaleiro.getRevistas()).toContain(revista)
+  })
+
+  it('Deve retornar o saldo da carteira', () => {
+    const carteira = new Carteira()
+    const pix = new Pix(100)
+    carteira.addMeioDePagamento(pix)
+    const jornaleiro = new Jornaleiro('João', carteira)
+    expect(jornaleiro.getSaldo()).toBe(100)
+  })
+
+  it('Deve retornar os meios de pagamento aceitos', () => {
+    const carteira = new Carteira()
+    const pix = new Pix(100)
+    carteira.addMeioDePagamento(pix)
+    const jornaleiro = new Jornaleiro('João', carteira)
+    expect(jornaleiro.getMeiosDePagamento()).toContain(pix)
   })
 })
